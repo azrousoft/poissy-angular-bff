77d575aa5e9c8bf3ea6d7f5177bc3cc41926c66c
Y.BEN keroum <azrousoft@yahoo.fr>
2023-08-21 16:01:26 +0200

    First branche

---
+++ src/app/state/event.driver.service.state.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { PageActionEvent } from "./produit.state";
+
+@Injectable(
+    {providedIn: "root"}
+)
+export class EventDriverService{
+    contratSourceEventSubject: Subject<PageActionEvent> = new Subject<PageActionEvent>();
+    contratSourceEventSubjectObservable = this.contratSourceEventSubject.asObservable();
+
+    /**
+     * 
+     * @param event Pour publier des event dans le subject
+     * Chaque component qui veut recevoir cet event doit subscribe to contratSourceEventSubjectObservable
+     */
+    publishEvent(event: PageActionEvent){
+        this.contratSourceEventSubject.next(event);
+    }
+
+}
